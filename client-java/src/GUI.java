7e73a2a22529a366a02bdee511b37db16010ebfa
Olle Kaiser <yogsther@gmail.com>
2019-04-09 00:12:07 +0200

    Java Client is now functional!

--- client-java/src/GUI.java
+++ client-java/src/GUI.java
@@ -1,20 +1,102 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class GUI extends JFrame {
 
-    private JLabel testLabel;
+    private static BufferedImage image = null;
+    private static BufferedImage convertedImage = null;
+    private static JPanel imagePreview;
+    private static JButton sendButton = new JButton();
+    private static JTextField ip = new JTextField();
+    private static JTextField key = new JTextField();
+    private static BufferedImage placeholder;
+    public static boolean displayPreview = false;
 
-    public GUI(){
+    public GUI() throws IOException {
 
-        add(testLabel);
+        int WINDOW_WIDTH = 500;
+        int WINDOW_HEIGHT = 550;
+
         setTitle("LIVE Client");
-        setSize(400, 500);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // TODO: Fix icon /(^-^)/ Exception in thread "main" java.lang.NullPointerException
         // at java.desktop/javax.swing.ImageIcon.<init>
         //setIconImage(new ImageIcon(getClass().getResource("img/icon.png")).getImage());
 
+        setIconImage(ImageIO.read(getClass().getResource("/img/logo.png")));
+        placeholder = ImageIO.read(getClass().getResource("/img/placeholder.png"));
+
+        imagePreview = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g){
+                super.paintComponent(g);
+                if(displayPreview){
+                    try {
+                        // Capture screenshot
+                        image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+                        convertedImage = main.resizeImg(image, 384, 216); // Scale down image
+                    } catch (AWTException e) {
+                        e.printStackTrace();
+                    }
+
+                    double factor = (double) convertedImage.getHeight() / convertedImage.getWidth(); // How much shorter the height is than the width
+
+                    // Draw preview of image
+                    g.drawImage(convertedImage, 0, 0, WINDOW_WIDTH, (int) Math.round(WINDOW_WIDTH * factor), null);
+                } else {
+                    double factor = (double) placeholder.getHeight() / placeholder.getWidth(); // How much shorter the height is than the width
+                    g.drawImage(placeholder, 0, 0,  WINDOW_WIDTH, (int) Math.round(WINDOW_WIDTH * factor), null);
+                }
+
+                repaint(); // Re-do this
+            }
+        };
+
+        //getContentPane().setBackground(Color.decode("#111111"));
+        // 500x550
+        setLayout(null);
+        imagePreview.setBounds(0, 0, 500, 300);
+        sendButton.setBounds(10, 450, 120, 40);
+        sendButton.setText("GO LIVE");
+
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    main.toggleStream();
+                } catch (URISyntaxException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        ip.setBounds(10, 320, 465, 30);
+        ip.setText("http://stream.ygstr.com");
+
+        key.setBounds(10, 360, 465, 30);
+        key.setText("KEY");
+
+        add(key);
+        add(ip);
+        add(sendButton);
+        add(imagePreview);
         setVisible(true);
+    }
+
+    public static String getKey(){
+        return key.getText();
+    }
+
+    public static BufferedImage getImage() {
+        return convertedImage;
     }
 }
--- client-java/src/main.java
+++ client-java/src/main.java
@@ -2,16 +2,80 @@ import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.net.URISyntaxException;
+import java.nio.Buffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class main {
-    public static void main(String[] args) throws URISyntaxException {
+    public static Socket socket;
+    public static boolean streaming = false;
+    public static Timer t;
+    public static TimerTask tt;
+
+    public static void main(String[] args) throws URISyntaxException, AWTException, IOException {
 
 
-        Socket socket = IO.socket("http://localhost");
-        socket.connect();
-
-        System.out.println("Test");
         GUI gui = new GUI();
     }
+
+    public static void toggleStream() throws URISyntaxException {
+        streaming = !streaming;
+
+        if(streaming){
+            // Start the stream
+            // Connect socket
+            socket = IO.socket("http://localhost");
+            socket.connect();
+
+            socket.emit("start_stream", GUI.getKey());
+
+            GUI.displayPreview = true;
+
+            t = new Timer();
+            tt = new TimerTask() {
+                @Override
+                public void run() {
+                    //do something
+                    BufferedImage img = GUI.getImage();
+
+                    if(img != null){
+                        try {
+
+                            final ByteArrayOutputStream os = new ByteArrayOutputStream();
+                            ImageIO.write(img, "png", Base64.getEncoder().wrap(os)); // Convert to base65-string
+                            socket.emit("stream", os.toString(StandardCharsets.ISO_8859_1.name()));
+
+                            } catch (final IOException ioe) {
+                        }
+                    }
+                }
+            };
+            t.schedule(tt,0,16);
+
+        } else {
+            t.cancel();
+            tt.cancel();
+            GUI.displayPreview = false;
+            socket.disconnect();
+        }
+    }
+
+    public static BufferedImage resizeImg(BufferedImage img, int newW, int newH) {
+        Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+        BufferedImage finalImg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D canvas = finalImg.createGraphics();
+        canvas.drawImage(tmp, 0, 0, null);
+        canvas.dispose();
+
+        return finalImg;
+    }
+
 }
